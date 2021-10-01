517cb8b4514f34541a84bf1c0eb048fd5cc1f7c2
m-yuhas <>
2021-10-01 23:44:21 +0800

    initial commit

---
+++ game_of_life.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+import cv2
+import torch
+import torch.nn.functional
+
+
+# Initialize the board randomly
+generation = 0
+X_SIZE, Y_SIZE = (100, 100)
+board = torch.rand(1, 1, X_SIZE, Y_SIZE)
+
+
+# Initialize our convolution kernel
+h = torch.tensor([[1.,  1.,  1.,],
+                  [1.,  0.5, 1.,],
+                  [1.,  1.,  1.,]]).view(1, 1, 3, 3)
+
+
+while generation < 10000:
+    # Apply convolution
+    board = torch.nn.functional.conv2d(board, h, padding=1)
+
+    # Apply activation function
+    board = torch.logical_and(board >= 2.5, board <= 3.5).float()
+
+    # Draw the current iteration
+    cv2.imshow('😎', cv2.resize(board.view(X_SIZE, Y_SIZE).numpy(), (400, 400)))
+    cv2.waitKey(10)
+
+
+cv2.waitKey(0)
